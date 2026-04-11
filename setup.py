447f53156f7ba95560eddeaeec7a0fdf4b6070bc
= <tthales08@gmail.com>
2026-04-11 16:02:35 -0300

    Adding uv and dockerfile

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="rag-study",
+    version="0.0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.12"
+)
